4a9d3d525190a9cf50a1f2a10d47e9db83fc474c
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-16 00:19:55 +0300

    commit tree 250f0deee1619e75a3090783f12071fb63c2133d

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,19 +6,50 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoods = [...foods, newFood];
+    setFoods(newFoods);
   }
 
+  // remove food from the list
+  // const foodList1 = foods.map((food) => (
+  //   <li 
+  //     key={food.id}
+  //     onClick={() => {
+  //       const newFoods = foods.filter((f) => f.id !== food.id);
+  //       setFoods(newFoods);
+  //     }
+  //     }
+  //   >
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+
+  // update using the click event on the list item to increase the heat level by 1
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li
+      key={food.id}
+      onClick={() => {
+        const newFoods = foods.map((f) => {
+          if (f.id === food.id) {
+            return { ...f, heatLevel: f.heatLevel + 1 };
+          }
+          return f;
+        }
+        );
+        setFoods(newFoods);
+      }
+      }
+    >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      {/* <ul>{foodList1}</ul> */}
     </div>
   );
 }
